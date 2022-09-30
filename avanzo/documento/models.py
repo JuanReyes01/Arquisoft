f743f23248874f80390d120a0208290c6e61b1e5
Juan Camilo <j.fallag@uniandes.edu.co>
2022-09-30 10:09:03 -0500

    modelo documento

--- avanzo/documento/models.py
+++ avanzo/documento/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from cliente.models import Cliente
 # Create your models here.
+class Documento(models.Model):
+    nombre = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=20)
+    #cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, default = None)
+    def __str__(self):
+        return self.nombre
