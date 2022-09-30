8014a8844ceb6f7b2fd790dda95b669406020ce2
Nicolás Klopstock <n.klopstock@uniandes.edu.co>
2022-09-30 14:56:03 -0500

    solicitud class creada

--- avanzo/solicitud/models.py
+++ avanzo/solicitud/models.py
@@ -1,3 +1,15 @@
+from pyexpat import model
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from cliente.models import Cliente
 
-# Create your models here.
+class Solicitud(models.Model):
+    monto = models.IntegerField()
+    interes = models.FloatField()
+    plazo = models.IntegerField()
+    estado = models.BooleanField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, default = None)
+
+    def __str__(self):
+        return '%s %s %s' % (self.cliente.pk, self.monto, self.estado)
+
