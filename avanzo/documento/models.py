aff9f5f1a29b800d4bb020ec4dae516b76934f3c
Juan Camilo <j.fallag@uniandes.edu.co>
2022-09-30 10:25:07 -0500

    admin y modelos de documento

--- avanzo/documento/models.py
+++ avanzo/documento/models.py
@@ -4,6 +4,9 @@ from cliente.models import Cliente
 class Documento(models.Model):
     nombre = models.CharField(max_length=100)
     tipo = models.CharField(max_length=20)
+    fecha = models.DateField()
+    saldo = models.DecimalField(max_digits=10, decimal_places=2)
+    cuentaBancaria = models.DecimalField(max_digits=15)
     #cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, default = None)
     def __str__(self):
         return self.nombre
