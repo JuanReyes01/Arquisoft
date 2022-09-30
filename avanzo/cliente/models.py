90ef8d29ad39c4e0babdb710980833a319b3754e
Juan Camilo Reyes <j.reyesc@uniandes.edu.co>
2022-09-30 10:10:31 -0500

    falla mucha perra

--- avanzo/cliente/models.py
+++ avanzo/cliente/models.py
@@ -1,3 +1,13 @@
+from statistics import correlation
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    cedula = models.IntegerField(null = False, blank = False, unique = True)
+    nombre = models.CharField(max_length = 50, null = False, blank = False)
+    telefono = models.IntegerField(null = False, blank = False, unique = True)
+    correo = models.CharField(max_length = 50, null = False, blank = False, unique = True)
+    direccion = models.CharField(max_length = 50, null = False, blank = False)
+    password = models.CharField(max_length = 50, null = False, blank = False)
+    
+    def __str__(self):
+        return '%s'+': '+ '%s' % (self.nombre,self.cedula)
