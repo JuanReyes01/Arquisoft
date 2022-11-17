17c3235a2f364e96bcf0ece31e897614600d3e79
Juan Camilo <j.fallag@uniandes.edu.co>
2022-11-17 13:37:32 -0500

    auth updates

--- avanzo/avanzo/settings.py
+++ avanzo/avanzo/settings.py
@@ -133,7 +133,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = "/login/auth0"
 LOGIN_REDIRECT_URL = "/" 
 LOGOUT_REDIRECT_URL = "https://isis2503-ivan-alfonso.auth0.com/v2/logout?returnTo=http%3A%2F%2Fip_publica_instancia:8080" 
-SOCIAL_AUTH_TRAILING_SLASH = False # Remove end slash from routes SOCIAL_AUTH_AUTH0_DOMAIN = 'isis2503-ivan-alfonso.auth0.com' SOCIAL_AUTH_AUTH0_KEY = 'W8g5KLG4s2ogftLqVDrGwd3xD7JafO0S' SOCIAL_AUTH_AUTH0_SECRET = '7MVp47TDXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' 
+SOCIAL_AUTH_TRAILING_SLASH = False # Remove end slash from routes 
+SOCIAL_AUTH_AUTH0_DOMAIN = 'isis2503-ivan-alfonso.auth0.com' 
+SOCIAL_AUTH_AUTH0_KEY = 'W8g5KLG4s2ogftLqVDrGwd3xD7JafO0S' 
+SOCIAL_AUTH_AUTH0_SECRET = '7MVp47TDXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' 
 SOCIAL_AUTH_AUTH0_SCOPE = [ 
                             'openid', 
                             'profile',
