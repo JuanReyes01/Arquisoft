19ff27dc361f78cabee6830067e74e7da9e73599
Alejandro  Salgado <78111596+Alejandro1400@users.noreply.github.com>
2022-11-18 16:13:47 -0500

    Update settings.py

--- avanzo/avanzo/settings.py
+++ avanzo/avanzo/settings.py
@@ -98,8 +98,12 @@ LOGGING = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'monitoringDB',
+        'USER': 'avanzo',
+        'PASSWORD': 'isis2503',
+        'HOST': 'monitoring-db.c0kddpauyqft.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
